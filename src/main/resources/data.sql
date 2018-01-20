0ff80d75216be05372bc470d5fccc68d4091dbaa
Preston Sheldon <pksiv@hotmail.com>
2018-01-20 08:31:05 -0500

    update for deployment to pcf

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,4 +1,4 @@
-insert into User (id, created, email, password) values (1, NOW(), 'john@test.com', '123');
-insert into User (id, created, email, password) values (2, NOW(), 'tom@test.com', 'abc');
-insert into User (id, created, email, password) values (3, NOW(), 'jane@test.com', 'test');
-insert into User (id, created, email, password) values (4, NOW(), 'psheldon@pivotal.io', 'pass');
+insert into user (id, created, email, password) values (1, NOW(), 'john@test.com', '123');
+insert into user (id, created, email, password) values (2, NOW(), 'tom@test.com', 'abc');
+insert into user (id, created, email, password) values (3, NOW(), 'jane@test.com', 'test');
+insert into user (id, created, email, password) values (4, NOW(), 'psheldon@pivotal.io', 'pass');
